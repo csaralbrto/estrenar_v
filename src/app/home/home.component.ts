46eea6caa4de076e1509bf5ded72c19acf0a0535
kpintorg <kpintorg@pop-os.localdomain>
2020-07-16 14:44:40 -0500

    Se sube primer commit de desarrollo front de pagina home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  name_company = 'Estrena Vivienda';
+  menu_title_proyects = 'Proyectos';
+  menu_title_house_VIS = 'Vivienda VIS';
+  menu_title_builders = 'Constructoras';
+  menu_title_tools = 'Herramientas';
+  menu_title_blog = 'Blog';
+  main_title_page = 'Te ayudamos a encontrar tu lugar ideal';
+  title_section_list_search = '¿Qué estás buscando?';
+  title_section_top_proyects = 'Proyectos destacados';
+  title_section_builders = 'Constructoras';
+  title_section_blog = 'Blog';
+  number_persons = 5;
 
   ngOnInit(): void {
-  	$('app-home').foundation();
+    $('app-home').foundation();
   }
-
 }
