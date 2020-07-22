ce1ae05499d7ba5d506b29cb0baf17032327214d
root <root@pop-os.localdomain>
2020-07-22 12:07:58 -0500

    Se ajusta el routing, se carga doble vez el app component duplicando el template de home, se ajusta y actualiza la redirección

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
