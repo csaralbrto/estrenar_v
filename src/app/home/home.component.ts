a17659384442d851ee876881e4b701a776a38fd5
root <root@pop-os.localdomain>
2020-07-22 09:13:49 -0500

    Se agrega template y estilos de header y footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,18 +14,11 @@ export class HomeComponent implements OnInit {
 
   constructor(public Service: HomeService) {}
 
-  name_company = 'Estrena Vivienda';
-  menu_title_proyects = 'Proyectos';
-  menu_title_house_VIS = 'Vivienda VIS';
-  menu_title_builders = 'Constructoras';
-  menu_title_tools = 'Herramientas';
-  menu_title_blog = 'Blog';
-  main_title_page = 'Te ayudamos a encontrar tu lugar ideal';
   title_section_list_search = '¿Qué estás buscando?';
   title_section_top_proyects = 'Proyectos destacados';
   title_section_builders = 'Constructoras';
   title_section_blog = 'Blog';
-  number_persons = 5;
+  main_title_page = 'Te ayudamos a encontrar tu lugar ideal';
 
   ngOnInit(): void {
     $('app-home').foundation();
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  name_company = 'Estrena Vivienda';
+  menu_title_proyects = 'Proyectos';
+  menu_title_house_VIS = 'Vivienda VIS';
+  menu_title_builders = 'Constructoras';
+  menu_title_tools = 'Herramientas';
+  menu_title_blog = 'Blog';
+  number_persons = 5;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
