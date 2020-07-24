9bb589886600206b759d2a15952f04f0bda7515e
root <root@pop-os.localdomain>
2020-07-24 14:41:54 -0500

    Agrego estructura sección, redirección modulo de proyecto e cambios en header y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'proyectos', component: ProjectsComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,11 +15,11 @@ export class HomeComponent implements OnInit {
 
   constructor(public Service: HomeService) {}
   dataPath = environment.endpoint;
-  cadena = "";
-  largo = "";
+  cadena = '';
+  largo = '';
 
   title_section_list_search = '¿Qué estás buscando?';
-  title_section_top_proyects = 'Proyectos destacados';
+  title_section_top_proyects = 'Proyectos destaca|dos';
   title_section_builders = 'Constructoras';
   title_section_blog = 'Blog';
   main_title_page = 'Te ayudamos a encontrar tu lugar ideal';
@@ -35,54 +35,54 @@ export class HomeComponent implements OnInit {
         if (this.response) {
           console.log(this.response.blogs);
           for (let blog of this.response.blogs) {
-            this.largo = blog.banner.length
+            this.largo = blog.banner.length;
             this.cadena = blog.banner.substr(23, this.largo);
-                   blog.banner = this.dataPath+this.cadena;
-         }
+            blog.banner = this.dataPath + this.cadena;
+          }
           for (let constructora of this.response.constructoras) {
-              if (constructora.logo) {
-                this.largo = constructora.logo.length
-                this.cadena = constructora.logo.substr(23, this.largo);
-                constructora.logo = this.dataPath+this.cadena;
+            if (constructora.logo) {
+              this.largo = constructora.logo.length;
+              this.cadena = constructora.logo.substr(23, this.largo);
+              constructora.logo = this.dataPath + this.cadena;
             }
           }
           for (let project of this.response.projects) {
             if (project.imagen_banner) {
-              this.largo = project.imagen_banner.length
+              this.largo = project.imagen_banner.length;
               this.cadena = project.imagen_banner.substr(23, this.largo);
-              project.imagen_banner = this.dataPath+this.cadena;
-              }
-          } 
+              project.imagen_banner = this.dataPath + this.cadena;
+            }
+          }
           for (let header_proj1 of this.response.header_proj1) {
-              if (header_proj1.url_img) {
-                this.largo = header_proj1.url_img.length
-                this.cadena = header_proj1.url_img.substr(23, this.largo);
-                header_proj1.url_img = this.dataPath+this.cadena;
+            if (header_proj1.url_img) {
+              this.largo = header_proj1.url_img.length;
+              this.cadena = header_proj1.url_img.substr(23, this.largo);
+              header_proj1.url_img = this.dataPath + this.cadena;
             }
           }
           for (let header_proj2 of this.response.header_proj2) {
-              if (header_proj2.url_img) {
-                this.largo = header_proj2.url_img.length
-                this.cadena = header_proj2.url_img.substr(23, this.largo);
-                header_proj2.url_img = this.dataPath+this.cadena;
+            if (header_proj2.url_img) {
+              this.largo = header_proj2.url_img.length;
+              this.cadena = header_proj2.url_img.substr(23, this.largo);
+              header_proj2.url_img = this.dataPath + this.cadena;
             }
           }
           for (let header_proj3 of this.response.header_proj3) {
-              if (header_proj3.url_img) {
-                this.largo = header_proj3.url_img.length
-                this.cadena = header_proj3.url_img.substr(23, this.largo);
-                header_proj3.url_img = this.dataPath+this.cadena;
+            if (header_proj3.url_img) {
+              this.largo = header_proj3.url_img.length;
+              this.cadena = header_proj3.url_img.substr(23, this.largo);
+              header_proj3.url_img = this.dataPath + this.cadena;
             }
           }
           for (let header_proj4 of this.response.header_proj4) {
-              if (header_proj4.url_img) {
-                this.largo = header_proj4.url_img.length
-                this.cadena = header_proj4.url_img.substr(23, this.largo);
-                header_proj4.url_img = this.dataPath+this.cadena;
+            if (header_proj4.url_img) {
+              this.largo = header_proj4.url_img.length;
+              this.cadena = header_proj4.url_img.substr(23, this.largo);
+              header_proj4.url_img = this.dataPath + this.cadena;
             }
           }
         }
-          /* si responde correctamente */
+        /* si responde correctamente */
         if (this.response.error) {
           /* si hay error en la respuesta */
         }
