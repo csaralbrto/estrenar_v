63ce4c0cfccb112730016bbed969d4145c9d1a03
csaralbrto <csaralbrto@gmail.com>
2022-07-01 15:40:05 -0500

    changes 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EstrenarVivienda
+# EstrenarV
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,50 @@
 export const environment = {
-  production: true
+  production: true,
+  site: 'https://estrenarvivienda.demodayscript.com/',
+  autor: '',
+  title: '',
+  description : '',
+  key_words: '',
+  image: '',
+  path : 'https://estrenarvivienda.demodayscript.com/',
+  type: 'website',
+  endpoint: 'https://api-estrenarvivienda.demodayscript.com/',
+  endpointApi: 'https://api-estrenarvivienda.demodayscript.com/api/',
+  endpointApiBasicPage: 'https://lab.estrenarvivienda.com/router/translate-path?path=',
+  endpointSearchApi: 'https://api-estrenarvivienda.demodayscript.com/api/search?',
+  endpointTesting: 'https://api-estrenarvivienda.demodayscript.com/api/',
+  endpointTestingApi: 'https://lab.estrenarvivienda.com/api/',
+  endpointTestingApiAdServer: 'https://ads.estrenarvivienda.com/www/delivery/asyncspc.php?zones=',
+  endpointTestingApiPost: 'https://lab.estrenarvivienda.com/',
+  endpointTestingApiUrl: 'https://lab.estrenarvivienda.com',
+  endpointTestingApiElastic: 'http://test-blaa.demodayscript.com/api/elasticsearch?',
+  not_metas:
+    [{
+      "tag": "link",
+      "attributes": {
+        "rel": "canonical",
+        "href": ""
+      }
+    },
+    {
+      "tag": "meta",
+      "attributes": {
+        "name": "title",
+        "content": "404"
+      }
+    },
+    {
+      "tag": "meta",
+      "attributes": {
+        "name": "description",
+        "content": ""
+      }
+    },
+    {
+    "tag": "meta",
+      "attributes": {
+      "name": "abstract",
+      "content": ""
+      }
+    }],
 };
